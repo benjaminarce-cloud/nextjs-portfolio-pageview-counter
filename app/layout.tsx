4c19f66c031a9a88872d505c0da1269902eedf61
benjaminarce-cloud <arceb3013@gmail.com>
2025-12-17 07:10:05 -0800

    Update site metadata for title and template

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Benjamin Arce",
+    template: "%s | Benjamin Arce",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
