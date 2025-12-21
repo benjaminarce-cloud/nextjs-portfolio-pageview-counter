66b5abba938aee29d17d16156b6b4df8596c4490
benjaminarce-cloud <arceb3013@gmail.com>
2025-12-21 10:52:12 -0800

    Add 'Log' navigation item to the menu

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Particles from "./components/particles";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
+  { name: "Log", href: "/log" }, 
   { name: "Contact", href: "/contact" },
 ];
 
